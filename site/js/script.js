e774dcb28af72f5af92288dfb97967945adb0472
Fábio Costa <fabio.costa.external@k2partnering.com>
2024-01-08 14:58:32 -0300

    First commit

---
+++ site/js/script.js
@@ -0,0 +1,367 @@
+"use strict";
+(function () {
+	// Global variables
+	var
+		userAgent = navigator.userAgent.toLowerCase(),
+		initialDate = new Date(),
+
+		$document = $(document),
+		$window = $(window),
+		$html = $("html"),
+		$body = $("body"),
+
+		isDesktop = $html.hasClass("desktop"),
+		isIE = userAgent.indexOf("msie") !== -1 ? parseInt(userAgent.split("msie")[1], 10) : userAgent.indexOf("trident") !== -1 ? 11 : userAgent.indexOf("edge") !== -1 ? 12 : false,
+		isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent),
+		windowReady = false,
+		isNoviBuilder = false,
+		livedemo = true,
+
+		plugins = {
+			copyrightYear:           $( '.copyright-year' ),
+			preloader:               $( '.preloader' ),
+			rdMailForm:              $( '.rd-mailform' ),
+			rdInputLabel:            $( '.form-label' ),
+			regula:                  $( '[data-constraints]' ),
+			wow:                     $( '.wow' ),
+			selectFilter:            $("select")
+		};
+
+	// Initialize scripts that require a loaded window
+	$window.on('load', function () {
+		// Page loader & Page transition
+		if (plugins.preloader.length && !isNoviBuilder) {
+			pageTransition({
+				target: document.querySelector( '.page' ),
+				delay: 0,
+				duration: 500,
+				classIn: 'fadeIn',
+				classOut: 'fadeOut',
+				classActive: 'animated',
+				conditions: function (event, link) {
+					return link && !/(\#|javascript:void\(0\)|callto:|tel:|mailto:|:\/\/)/.test(link) && !event.currentTarget.hasAttribute('data-lightgallery');
+				},
+				onTransitionStart: function ( options ) {
+					setTimeout( function () {
+						plugins.preloader.removeClass('loaded');
+					}, options.duration * .75 );
+				},
+				onReady: function () {
+					plugins.preloader.addClass('loaded');
+					windowReady = true;
+				}
+			});
+		}
+	});
+
+	// Initialize scripts that require a finished document
+	$(function () {
+		isNoviBuilder = window.xMode;
+
+		/**
+		 * @desc Attach form validation to elements
+		 * @param {object} elements - jQuery object
+		 */
+		function attachFormValidator(elements) {
+			// Custom validator - phone number
+			regula.custom({
+				name: 'PhoneNumber',
+				defaultMessage: 'Invalid phone number format',
+				validator: function() {
+					if ( this.value === '' ) return true;
+					else return /^(\+\d)?[0-9\-\(\) ]{5,}$/i.test( this.value );
+				}
+			});
+
+			for (var i = 0; i < elements.length; i++) {
+				var o = $(elements[i]), v;
+				o.addClass("form-control-has-validation").after("<span class='form-validation'></span>");
+				v = o.parent().find(".form-validation");
+				if (v.is(":last-child")) o.addClass("form-control-last-child");
+			}
+
+			elements.on('input change propertychange blur', function (e) {
+				var $this = $(this), results;
+
+				if (e.type !== "blur") if (!$this.parent().hasClass("has-error")) return;
+				if ($this.parents('.rd-mailform').hasClass('success')) return;
+
+				if (( results = $this.regula('validate') ).length) {
+					for (i = 0; i < results.length; i++) {
+						$this.siblings(".form-validation").text(results[i].message).parent().addClass("has-error");
+					}
+				} else {
+					$this.siblings(".form-validation").text("").parent().removeClass("has-error")
+				}
+			}).regula('bind');
+
+			var regularConstraintsMessages = [
+				{
+					type: regula.Constraint.Required,
+					newMessage: "The text field is required."
+				},
+				{
+					type: regula.Constraint.Email,
+					newMessage: "The email is not a valid email."
+				},
+				{
+					type: regula.Constraint.Numeric,
+					newMessage: "Only numbers are required"
+				},
+				{
+					type: regula.Constraint.Selected,
+					newMessage: "Please choose an option."
+				}
+			];
+
+
+			for (var i = 0; i < regularConstraintsMessages.length; i++) {
+				var regularConstraint = regularConstraintsMessages[i];
+
+				regula.override({
+					constraintType: regularConstraint.type,
+					defaultMessage: regularConstraint.newMessage
+				});
+			}
+		}
+
+		/**
+		 * @desc Check if all elements pass validation
+		 * @param {object} elements - object of items for validation
+		 * @param {object} captcha - captcha object for validation
+		 * @return {boolean}
+		 */
+		function isValidated(elements, captcha) {
+			var results, errors = 0;
+
+			if (elements.length) {
+				for (var j = 0; j < elements.length; j++) {
+
+					var $input = $(elements[j]);
+					if ((results = $input.regula('validate')).length) {
+						for (k = 0; k < results.length; k++) {
+							errors++;
+							$input.siblings(".form-validation").text(results[k].message).parent().addClass("has-error");
+						}
+					} else {
+						$input.siblings(".form-validation").text("").parent().removeClass("has-error")
+					}
+				}
+
+				if (captcha) {
+					if (captcha.length) {
+						return validateReCaptcha(captcha) && errors === 0
+					}
+				}
+
+				return errors === 0;
+			}
+			return true;
+		}
+
+		// Additional class on html if mac os.
+		if (navigator.platform.match(/(Mac)/i)) {
+			$html.addClass("mac-os");
+		}
+
+		// Adds some loosing functionality to IE browsers (IE Polyfills)
+		if (isIE) {
+			if (isIE === 12) $html.addClass("ie-edge");
+			if (isIE === 11) $html.addClass("ie-11");
+			if (isIE < 10) $html.addClass("lt-ie-10");
+			if (isIE < 11) $html.addClass("ie-10");
+		}
+
+		// Copyright Year (Evaluates correct copyright year)
+		if (plugins.copyrightYear.length) {
+			plugins.copyrightYear.text(initialDate.getFullYear());
+		}
+
+		// UI To Top
+		if (isDesktop && !isNoviBuilder) {
+			$().UItoTop({
+				easingType: 'easeOutQuad',
+				containerClass: 'ui-to-top fa fa-angle-up'
+			});
+		}
+
+		// WOW
+		if ($html.hasClass("wow-animation") && plugins.wow.length && !isNoviBuilder && isDesktop) {
+			new WOW().init();
+		}
+
+		// RD Input Label
+		if (plugins.rdInputLabel.length) {
+			plugins.rdInputLabel.RDInputLabel();
+		}
+
+		// Regula
+		if (plugins.regula.length) {
+			attachFormValidator(plugins.regula);
+		}
+
+		// RD Mailform
+		if (plugins.rdMailForm.length) {
+			var i, j, k,
+				msg = {
+					'MF000': 'Successfully sent!',
+					'MF001': 'Recipients are not set!',
+					'MF002': 'Form will not work locally!',
+					'MF003': 'Please, define email field in your form!',
+					'MF004': 'Please, define type of your form!',
+					'MF254': 'Something went wrong with PHPMailer!',
+					'MF255': 'Aw, snap! Something went wrong.'
+				};
+
+			for (i = 0; i < plugins.rdMailForm.length; i++) {
+				var $form = $(plugins.rdMailForm[i]),
+					formHasCaptcha = false;
+
+				$form.attr('novalidate', 'novalidate').ajaxForm({
+					data: {
+						"form-type": $form.attr("data-form-type") || "contact",
+						"counter": i
+					},
+					beforeSubmit: function (arr, $form, options) {
+						if (isNoviBuilder)
+							return;
+
+						var form = $(plugins.rdMailForm[this.extraData.counter]),
+							inputs = form.find("[data-constraints]"),
+							output = $("#" + form.attr("data-form-output")),
+							captcha = form.find('.recaptcha'),
+							captchaFlag = true;
+
+						output.removeClass("active error success");
+
+						if (isValidated(inputs, captcha)) {
+
+							// veify reCaptcha
+							if (captcha.length) {
+								var captchaToken = captcha.find('.g-recaptcha-response').val(),
+									captchaMsg = {
+										'CPT001': 'Please, setup you "site key" and "secret key" of reCaptcha',
+										'CPT002': 'Something wrong with google reCaptcha'
+									};
+
+								formHasCaptcha = true;
+
+								$.ajax({
+									method: "POST",
+									url: "bat/reCaptcha.php",
+									data: {'g-recaptcha-response': captchaToken},
+									async: false
+								})
+									.done(function (responceCode) {
+										if (responceCode !== 'CPT000') {
+											if (output.hasClass("snackbars")) {
+												output.html('<p><span class="icon text-middle mdi mdi-check icon-xxs"></span><span>' + captchaMsg[responceCode] + '</span></p>')
+
+												setTimeout(function () {
+													output.removeClass("active");
+												}, 3500);
+
+												captchaFlag = false;
+											} else {
+												output.html(captchaMsg[responceCode]);
+											}
+
+											output.addClass("active");
+										}
+									});
+							}
+
+							if (!captchaFlag) {
+								return false;
+							}
+
+							form.addClass('form-in-process');
+
+							if (output.hasClass("snackbars")) {
+								output.html('<p><span class="icon text-middle fa fa-circle-o-notch fa-spin icon-xxs"></span><span>Sending</span></p>');
+								output.addClass("active");
+							}
+						} else {
+							return false;
+						}
+					},
+					error: function (result) {
+						if (isNoviBuilder)
+							return;
+
+						var output = $("#" + $(plugins.rdMailForm[this.extraData.counter]).attr("data-form-output")),
+							form = $(plugins.rdMailForm[this.extraData.counter]);
+
+						output.text(msg[result]);
+						form.removeClass('form-in-process');
+
+						if (formHasCaptcha) {
+							grecaptcha.reset();
+						}
+					},
+					success: function (result) {
+						if (isNoviBuilder)
+							return;
+
+						var form = $(plugins.rdMailForm[this.extraData.counter]),
+							output = $("#" + form.attr("data-form-output")),
+							select = form.find('select');
+
+						form
+							.addClass('success')
+							.removeClass('form-in-process');
+
+						if (formHasCaptcha) {
+							grecaptcha.reset();
+						}
+
+						result = result.length === 5 ? result : 'MF255';
+						output.text(msg[result]);
+
+						if (result === "MF000") {
+							if (output.hasClass("snackbars")) {
+								output.html('<p><span class="icon text-middle mdi mdi-check icon-xxs"></span><span>' + msg[result] + '</span></p>');
+							} else {
+								output.addClass("active success");
+							}
+						} else {
+							if (output.hasClass("snackbars")) {
+								output.html(' <p class="snackbars-left"><span class="icon icon-xxs mdi mdi-alert-outline text-middle"></span><span>' + msg[result] + '</span></p>');
+							} else {
+								output.addClass("active error");
+							}
+						}
+
+						form.clearForm();
+
+						if (select.length) {
+							select.val(null).trigger("change");
+						}
+
+						form.find('input, textarea').trigger('blur');
+
+						setTimeout(function () {
+							output.removeClass("active error success");
+							form.removeClass('success');
+						}, 3500);
+					}
+				});
+			}
+		}
+
+		// Select 2
+		if ( plugins.selectFilter.length ) {
+			for ( var i = 0; i < plugins.selectFilter.length; i++ ) {
+				var select = $( plugins.selectFilter[ i ] );
+
+				select.select2( {
+					dropdownParent:          $( '.page' ),
+					placeholder:             select.attr( 'data-placeholder' ) || null,
+					minimumResultsForSearch: select.attr( 'data-minimum-results-search' ) || Infinity,
+					containerCssClass:       select.attr( 'data-container-class' ) || null,
+					dropdownCssClass:        select.attr( 'data-dropdown-class' ) || null
+				} );
+			}
+		}
+	});
+}());
